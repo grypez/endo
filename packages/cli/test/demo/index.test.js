2ba014228209e721101d0398cf41be2cb0e6bb2b
grypez <143971198+grypez@users.noreply.github.com>
2024-07-22 11:15:40 -0400

    test(cli): Add first section test

--- packages/cli/test/demo/index.test.js
+++ packages/cli/test/demo/index.test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 import { $ } from 'execa';
 import { wrapSectionTest } from './section.js';
+import * as counterExample from './counter-example.js';
 
 test.before(async () => $`alias endo=$PWD/bin/endo`);
 
@@ -8,3 +9,11 @@ test.serial('trivial (test of test)', wrapSectionTest($({ cwd: 'demo' }), {}, as
   const maxim = 'a failing test is better than failure to test';
   await testLine(execa`echo ${maxim}`, { stdout: maxim });
 }));
+
+test.serial('fixtures', wrapSectionTest($({ cwd: 'demo' }), counterExample.fixture, async (execa, testLine) => {
+  await testLine(execa`endo show counter`, {
+    stdout: 'Object [Alleged: Counter] {}'
+  });
+}));
+
+test.serial('counter-example', wrapSectionTest($({ cwd: 'demo' }), {}, counterExample.section));
